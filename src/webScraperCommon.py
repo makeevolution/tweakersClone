23a3fba120fcab487e871eccfa7f645fb17ba89c
makeevolution <aldo_hasibuan@yahoo.com>
2021-12-19 12:46:15 +0100

    add debug print statements for driver

--- src/webScraperCommon.py
+++ src/webScraperCommon.py
@@ -180,7 +180,9 @@ class helperFunctions():
         firefox_options.add_argument("--headless")
         if OnServer:
             os.system("DISPLAY=:1 firefox")
+            print("assign driver")
             driver = webdriver.Firefox(executable_path=pwd + r"/geckodriver",options=firefox_options)
+            print("assign driver complete")
         else:
             driver = webdriver.Firefox(executable_path=pwd + r"/geckodriver.exe",options=firefox_options)
 
